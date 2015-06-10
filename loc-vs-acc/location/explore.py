31c5a91b95e03012e52427ff8d2a9b430188e359
heziresheff <yehezkel.resheff@mail.huji.ac.il>
2015-06-10 19:41:49 +0300

    computation of time, speed, distance between points

--- loc-vs-acc/location/explore.py
+++ loc-vs-acc/location/explore.py
@@ -4,14 +4,36 @@ import numpy as np
 import settings as st 
 from util import *
 
+def compute_steps(frame):
+    """ Compute the distance time and speed between points.
+    :param frame: Each row is a point. 
+    """
+    f = frame
+    f1 = f.shift(1)
+        
+    data = [
+        { "time": f.loc[ix, "stamp"] - f1.loc[ix, "stamp"],
+           "dist": equirectangular_approx_distance(f.loc[ix, "gps_lat"], f.loc[ix, "gps_long"], f1.loc[ix, "gps_lat"], f1.loc[ix, "gps_long"])
+        }
+        for ix in f.index[1:]]
+    for p in data:    
+        p["speed"] = p["dist"] / (p["time"].total_seconds() / pd.Timedelta("1h").total_seconds())
+    
+    return pd.DataFrame(data, index=f.index[1:])
+
+
 def step_dist(file_name):
     data = load_gps_csv(file_name)
     for animal_id, animal_data in data.groupby(data.bird_id):        
-        print(animal_id)        
-        print(animal_data[['date_start_fix', 'time_start_fix']])
-    
-
-
+        
+        animal_data['stamp'] = animal_data.apply(lambda row: row.date_start_fix + pd.Timedelta(row.time_start_fix), axis=1)        
+        data = compute_steps(animal_data[["gps_lat", "gps_long", "stamp"]])
+       
+      
+        print(data)
+            
+        
+        
 
 
 if __name__ == "__main__":
--- loc-vs-acc/location/util.py
+++ loc-vs-acc/location/util.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import settings as st
+from math import cos, sqrt, radians
 
 def load_gps_csv(file_name, min_len=10):
     data = pd.DataFrame.from_csv(os.path.join(st.DATA_ROOT, file_name), parse_dates=['date_start_fix'])
@@ -13,7 +14,20 @@ def load_gps_csv(file_name, min_len=10):
     data  = data.loc[data['bird_id'].isin(good_aninmals)]   
     return data 
 
-if __name__ == "__main__":
-    data = load_gps_csv("storks_gps_Jan2012.csv")
-    print(data)
+def equirectangular_approx_distance(lat1, lon1, lat2, lon2):
+    """
+    http://www.movable-type.co.uk/scripts/latlong.html
+    approx distance between GPS coordinates (not bad for small distances)
+    """
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+    R = 6371 # radius of the earth in km
+    x = (lon2 - lon1) * cos( 0.5*(lat2+lat1) )
+    y = lat2 - lat1
+    d = R * sqrt( x*x + y*y )
+    return d
 
+if __name__ == "__main__":
+    #data = load_gps_csv("storks_gps_Jan2012.csv")
+    #print(data)
+    d = equirectangular_approx_distance(40.712784, -74.005941,  40.628546, -73.927689)
+    print(d)
