de56d6b41d6d6aadc2ac8b205dbeff854eee2b72
heziresheff <yehezkel.resheff@mail.huji.ac.il>
2015-06-11 21:50:56 +0300

    simple colored traj clusters

--- loc-vs-acc/location/explore.py
+++ loc-vs-acc/location/explore.py
@@ -23,6 +23,23 @@ def trajectories(file_name):
         print(animal_id)
         animal_data.plot(x="gps_long", y="gps_lat", style="-x")
         plt.show()
+
+def traj_clustres(file_name):
+    for animal_id, animal_data in iter_animal(file_name, cols=["gps_lat", "gps_long", "stamp"], stamp=True):
+        data = compute_steps(animal_data)
+        data = trajectory_cluster(data, "speed", k=3)                
+        data["long"] = animal_data.gps_long[1:].values
+        data["lat"] = animal_data.gps_lat[1:].values
+        
+        #ax = data.plot(x='long', y='lat', style="-", color='black')
+        plt.hold(True)
+        for i in range(len(data)-1):                        
+            c = list("rgb")[data.iloc[i+1].cluster] 
+            plt.plot([data.iloc[i].long, data.iloc[i+1].long], [data.iloc[i].lat, data.iloc[i+1].lat], color=c, linestyle='-', linewidth=1)
+                   
+        plt.show()
+        
+        
                                     
 def speed_n_dist_hist(file_name):
     for animal_id, animal_data in iter_animal(file_name, cols=["gps_lat", "gps_long", "stamp"]):
@@ -44,4 +61,4 @@ if __name__ == "__main__":
     #cum_dist("storks_gps_Jan2012.csv")
     #trajectories("storks_gps_Jan2012.csv")
     #speed_n_dist_hist("storks_gps_Jan2012.csv")
-    time_delta("storks_gps_Jan2012.csv")
+    traj_clustres("storks_gps_Jan2012.csv")
--- loc-vs-acc/location/location.py
+++ loc-vs-acc/location/location.py
@@ -2,6 +2,7 @@
 
 import pandas as pd 
 import numpy as np 
+from sklearn.cluster import KMeans
 
 from util import *
 
@@ -22,3 +23,15 @@ def compute_steps(frame):
         p["speed"] = p["dist"] / (p["time"].total_seconds() / pd.Timedelta("1h").total_seconds())
     
     return pd.DataFrame(data, index=f.stamp[1:])
+
+def trajectory_cluster(frame, target, k=3):
+    """Naive clustering of trajectory, based on the values of a single column.
+    :param frame: the data
+    :param target: teh name of the column to use for clustering
+    :param k: the number of clusters
+    """    
+    clusters = KMeans(n_clusters=k).fit_predict(np.atleast_2d(frame[target].values).T)
+    frame["cluster"] = clusters
+    return frame
+    
+    
