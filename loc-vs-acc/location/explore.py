2f2ec5f6d7392446e345050ea152110e57a96ba6
heziresheff <yehezkel.resheff@mail.huji.ac.il>
2015-06-16 10:59:29 +0300

    trajectories go on Basemap

--- loc-vs-acc/location/explore.py
+++ loc-vs-acc/location/explore.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt 
+from mpl_toolkits.basemap import Basemap
 
 import settings as st 
 from util import *
@@ -24,6 +25,28 @@ def trajectories(file_name):
         animal_data.plot(x="gps_long", y="gps_lat", style="-x")
         plt.show()
 
+def traj_map(file_name):
+    for animal_id, animal_data in iter_animal(file_name, cols=["gps_lat", "gps_long"], stamp=False):  
+        params = {
+                'projection':'merc', 
+                'lat_0':animal_data.gps_lat.mean(), 
+                'lon_0':animal_data.gps_long.mean(), 
+                'resolution':'h', 
+                'area_thresh':0.1, 
+                'llcrnrlon':animal_data.gps_long.min()-10, 
+                'llcrnrlat':animal_data.gps_lat.min()-10, 
+                'urcrnrlon':animal_data.gps_long.max()+10, 
+                'urcrnrlat':animal_data.gps_lat.max()+10
+            }
+        map = Basemap(**params)
+        map.drawcoastlines()
+        map.drawcountries()
+        map.fillcontinents(color = 'coral')
+        map.drawmapboundary()          
+        x, y = map(animal_data.gps_long.values, animal_data.gps_lat.values)
+        map.plot(x, y, 'b-', linewidth=3)    
+        plt.show()
+                              
 def traj_clustres(file_name):
     for animal_id, animal_data in iter_animal(file_name, cols=["gps_lat", "gps_long", "stamp"], stamp=True):
         data = compute_steps(animal_data)
@@ -38,9 +61,7 @@ def traj_clustres(file_name):
             plt.plot([data.iloc[i].long, data.iloc[i+1].long], [data.iloc[i].lat, data.iloc[i+1].lat], color=c, linestyle='-', linewidth=1)
                    
         plt.show()
-        
-        
-                                    
+                                            
 def speed_n_dist_hist(file_name):
     for animal_id, animal_data in iter_animal(file_name, cols=["gps_lat", "gps_long", "stamp"]):
         data = compute_steps(animal_data)
@@ -57,8 +78,10 @@ def time_delta(file_name):
         lag[lag>21].hist(bins=100)
         plt.show()
 
+
 if __name__ == "__main__":
     #cum_dist("storks_gps_Jan2012.csv")
     #trajectories("storks_gps_Jan2012.csv")
     #speed_n_dist_hist("storks_gps_Jan2012.csv")
-    traj_clustres("storks_gps_Jan2012.csv")
+    #traj_clustres("storks_gps_Jan2012.csv")
+    traj_map("storks_gps_Jan2012.csv")
