34ccc0cd185cc4a5433e1862477a0c03e0adb6f9
heziresheff <yehezkel.resheff@mail.huji.ac.il>
2015-06-23 18:59:14 +0300

    temp map /w clusters

--- loc-vs-acc/location/temp.py
+++ loc-vs-acc/location/temp.py
@@ -2,23 +2,23 @@
 
 import pandas as pd 
 import matplotlib.pyplot as plt 
-from mpl_toolkits.basemap import Basemap
-
-class MyBasemap(Basemap):     
-    def printcountries(self, d=3, max_len=12):
-        data = pd.io.parsers.read_csv("http://opengeocode.org/cude/download.php?file=/home/fashions/public_html/opengeocode.org/download/cow.txt", 
-                                      sep=";", skiprows=28 )
-        data = data[(data.latitude > self.llcrnrlat+d) & (data.latitude < self.urcrnrlat-d) & (data.longitude > self.llcrnrlon+d) & (data.longitude < self.urcrnrlon-d)]
-        for ix, country in data.iterrows():                            
-                plt.text(*self(country.longitude, country.latitude), s=country.BGN_name[:max_len]) 
+from location import *
 
 
 path = "C:\\Users\\t-yeresh\\data\\storks2012\\storks_2012_GPS_sparse.csv"
 
-animal_data = pd.DataFrame.from_csv(path, header=None)
+# Load
+animal_data = pd.DataFrame.from_csv(path, header=None, parse_dates=[0, 1])
 animal_data.columns = ["date", "time", "gps_lat", "gps_long"]
+
+# select animal and claen
 animal_data = animal_data.loc[animal_data.index == 17870582]
 animal_data = animal_data[animal_data.gps_lat != 0]
+animal_data.reset_index(inplace=True) 
+
+#sort  by timestamp
+animal_data['stamp'] = animal_data.apply(lambda row: row.date + pd.Timedelta(row.time), axis=1)    
+animal_data.sort("stamp", inplace=True)
 
 params = {
         'projection':'merc', 
@@ -41,9 +41,12 @@ map.drawmapboundary()
 map.drawcountries()
 map.printcountries()
 
-x, y = map(animal_data.gps_long.values, animal_data.gps_lat.values)
+# cluster
+clst = trajectory_cluster(compute_steps(animal_data), "speed")["cluster"]
 
-map.plot(x, y, 'b-', linewidth=1)    
+# plot
+x, y = map(animal_data.gps_long.values[1:], animal_data.gps_lat.values[1:])
+map.plot(x, y, clst, linewidth=1)    
 plt.show()
 
 
