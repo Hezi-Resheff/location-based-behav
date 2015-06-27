4f5a58bb4054a044553f45670b7b8b896b5ae67c
Hezi Resheff <yehezkel.resheff@mail.huji.ac.il>
2015-06-27 15:58:03 +0300

    FPT

--- loc-vs-acc/location/location.py
+++ loc-vs-acc/location/location.py
@@ -3,21 +3,45 @@ import pandas as pd
 import numpy as np 
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt 
-from mpl_toolkits.basemap import Basemap
+#from mpl_toolkits.basemap import Basemap
+Basemap = object 
 
 from util import *
 
 class trajectory_processor(pd.DataFrame):
 
-    def __init__(self, data = None, index = None, columns = None, dtype = None, copy = False):
-        return super(trajectory_processor, self).__init__(data, index, columns, dtype, copy)
+    def __init__(self, data = None, index = None, columns = None, dtype = None, copy = False, stamp=True):
+        super(trajectory_processor, self).__init__(data, index, columns, dtype, copy)
+        if stamp:
+            self['stamp'] = self.apply(lambda row: row.date + pd.Timedelta(row.time), axis=1)    
+            self.drop(["date","time"], axis=1, inplace=True)
+        self.sort("stamp", inplace=True)
+        self.reset_index(drop=True, inplace=True)
 
     def compute_steps(self):
         return self
     
-    def compute_first_passage(self, radius, max_time=np.inf):
+    def compute_first_passage(self, radius, col_name=None):
+        """ For each data point, compute the time delta until it first crosses the boundry of the *radius* circle around it """
+        if col_name is None:
+            col_name = "FPT_" + str(radius)
+        self[col_name] = 0
+        
+        # TODO: find a better way of computing FPT!
+        N = len(self)
+        for i in range(N):  
+            j = i + 1
+            while j < N - 1:                                               
+                d = equirectangular_approx_distance(self.ix[i ,"gps_lat"], self.ix[i, "gps_long"], self.ix[j, "gps_lat"], self.ix[j, "gps_long"])
+                if d >= radius:                    
+                    self.ix[i, col_name] = self.ix[j, "stamp"] - self.ix[i, "stamp"]
+                    break
+                j += 1 
+            else:                
+                # end of data
+                self.ix[i, col_name] = np.NaN                   
         return self
-    
+
     def cluster(self, target=None):
         return self
     
